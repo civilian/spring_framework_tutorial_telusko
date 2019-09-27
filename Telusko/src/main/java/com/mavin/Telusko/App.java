cf4f72e6ba310cec4445016c8693cbcbcc146a87
Óscar Chamat <oscarchamat@useitweb.com>
2019-09-27 13:33:05 -0500

    Understanding maven.

---
+++ Telusko/src/main/java/com/mavin/Telusko/App.java
@@ -0,0 +1,14 @@
+package com.mavin.Telusko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Bike obj = new Bike();
+        obj.drive();
+    }
+}
---
+++ Telusko/src/main/java/com/mavin/Telusko/Car.java
@@ -0,0 +1,7 @@
+package com.mavin.Telusko;
+
+public class Car{
+    public void drive(){
+        System.out.println("Run run run ...");
+    }
+}
