f7047af55caba0472c4139c0e99cf4c060db2711
Óscar Chamat <oscarchamat@useitweb.com>
2019-10-21 13:54:48 -0500

    Adding context by annotations.

--- Telusko/src/main/java/com/mavin/Telusko/Main.java
+++ Telusko/src/main/java/com/mavin/Telusko/Main.java
@@ -12,7 +12,7 @@ public class Main
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Vehicle obj = (Vehicle) context.getBean("vehicle");
+        Vehicle obj = (Vehicle) context.getBean("car");
         obj.drive();
     }
 }
